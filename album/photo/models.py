c3b693a389c360d99fac5d58b6707a30a68c752b
Herbert Zhou <herbert.zhou@sioux.asia>
2022-01-19 17:53:40 +0800

    ok

--- album/photo/models.py
+++ album/photo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.timezone import now
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='photo/%Y%m%d/')
+    created = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.image.name
